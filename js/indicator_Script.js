53cc3ecb3f67f56b24499d98653d8401272cdd6d
Srinivasan Arumugam <srinivasan.arumugam1@wipro.com>
2016-05-24 09:29:11 +0000

    Json update

--- js/indicator_Script.js
+++ js/indicator_Script.js
@@ -5,7 +5,7 @@ var stream = require('stream');
 var ASIAN_COUNTRIES = ["Afghanistan", "Bahrain", "Bangladesh", "Bhutan", "Myanmar", "Cambodia", "China", "India", "Indonesia", "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Nepal",
 "Oman", "Pakistan", "Philippines", "Qatar", "Saudi Arabia", "Singapore", "Sri Lanka", "Syrian Arab Republic", "Tajikistan", "Thailand", "Timor-Leste", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam"];
 
-var instream = fs.ReadStream('../csv/Indicators.csv');
+var instream = fs.ReadStream('../../csv/Indicators.csv');
 var outstream1 = fs.WriteStream('urban_population.json');
 outstream1.readable = true;
 outstream1.writable = true;
@@ -40,7 +40,8 @@ rl.on('line', function(line) {
     //China,CHN,"Population, total",SP.POP.TOTL,1960,667070000.0
     if(currentline[countryIndex]==="India" && (currentline[indicatorCodeIndex]==="SP.RUR.TOTL.ZS" || currentline[indicatorCodeIndex]==="SP.URB.TOTL.IN.ZS")) {
       currentline = [currentline[countryIndex],currentline[year],currentline[value],currentline[indicatorCodeIndex]];
-      count1 = writeToFile(currentline,outstream1,count1);
+      count1 = writeToFilewithValue(currentline,outstream1,count1);
+      console.log(currentline);
     } else if(isInAsia = inAsia(currentline[countryIndex])) {
           if(currentline[indicatorCodeIndex]==="SP.RUR.TOTL" || currentline[indicatorCodeIndex]==="SP.URB.TOTL"){
           count3=writeToFile(currentline,outstream3,count3)
@@ -62,6 +63,32 @@ function inAsia(currentCountry){
   }
   return false;
 }
+function writeToFilewithValue(currentline,outstream,count) {
+  var obj = {};
+  var mod=0,counter=0;
+  for(var i=0;i<currentline.length;i++){
+    mod = counter %4;
+    if(mod == 0){
+       obj[headers[0]] = currentline[i];
+    } else if(mod == 1){
+       obj[headers[4]] = currentline[i];
+    }else if(mod == 2){
+       obj[headers[5]] = currentline[i];
+    }else {
+         obj[headers[3]] = currentline[i];
+    }
+   counter++;
+  }
+
+  if(count==1) {
+    outstream.write(JSON.stringify(obj));
+  } else {
+    outstream.write(","+JSON.stringify(obj));
+  }
+
+  count++;
+  return count;
+}
 function writeToFile(currentline,outstream,count) {
   var obj = {};
   for(var i=0;i<currentline.length;i++){
@@ -73,7 +100,6 @@ function writeToFile(currentline,outstream,count) {
   } else {
     outstream.write(","+JSON.stringify(obj));
   }
-
   count++;
   return count;
 }
